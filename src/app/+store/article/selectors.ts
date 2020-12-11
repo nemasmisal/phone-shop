e4d440326c512bd05be029ffa22a55a8e4200c95
nemasmisal <nikolay.topalov@gmail.com>
2020-12-11 16:57:21 +0200

    sorting top likes from asc to dsc ordering

--- src/app/+store/article/selectors.ts
+++ src/app/+store/article/selectors.ts
@@ -8,8 +8,4 @@ export const getAccessories = (state: IArticleState) => state.accessories;
 export const getArticleState = (state: IArticleState) => state;
 export const getCategory = (state: IArticleState, { name }) => state[name];
 export const getArticleById = (state: IArticleState, { category, articleId }) => state[category].find((x: IArticle) => x._id === articleId);
-export const topThreeLiked = (state: IArticleState, { category }) => {
-const categ =  state[category];
-const sorted = categ.slice(0,3)
-return sorted;
-};
+export const topThreeLiked = (state: IArticleState, { category }) => state[category].slice().sort((a: IArticle, b: IArticle) => b.likes.length - a.likes.length).slice(0, 3);
